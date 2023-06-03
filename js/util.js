ced6ca7a0844a29cbd20767bc5ad23c43996a8c3
Eugene <zeazagaynov2002@mail.ru>
2023-06-03 11:12:39 +0500

    added added calculation of calories in util.js

--- js/util.js
+++ js/util.js
@@ -1,5 +1,5 @@
-const LOSS_WEIGHT_NORM = 0.15;
-const GAIN_WEIGHT_NORM = 0.15;
+const LOSS_WEIGHT_RATE = 0.15;
+const GAIN_WEIGHT_RATE = 0.15;
 const ActivityRate = {
     "min": 1.2,
     "low": 1.375,
@@ -7,14 +7,14 @@ const ActivityRate = {
     "high": 1.725,
     "max": 1.9
 };
-const ValidParameters = {
+const ValidParameter = {
     AGE_MIN: 0,
-    AGE_MAX: 120,
-    HEIGHT_MIN: 45,
-    HEIGHT_MAX: 220,
-    WEIGHT_MIN: 4,
-    WEIGHT_MAX: 220
-};
+    AGE_MAX: 130,
+    HEIGHT_MIN: 60,
+    HEIGHT_MAX: 250,
+    WEIGHT_MIN: 2,
+    WEIGHT_MAX: 500
+}
 
 const ERROR_SHOW_TIME = 3000;
 
@@ -28,9 +28,13 @@ const showError = (message) => {
     alertContainer.style.padding = '0';
     alertContainer.style.fontSize = '40px';
     alertContainer.style.textAlign = 'center';
-    alertContainer.style.backgroundColor = 'red';
+    alertContainer.style.backgroundColor = 'orange';
     alertContainer.style.height = '130px';
     alertContainer.style.lineHeight = 'normal';
+    alertContainer.style.borderWidth = '5px';
+    alertContainer.style.borderStyle = 'solid';
+    alertContainer.style.borderColor = 'red';
+    alertContainer.style.borderRadius = '20px';
     alertContainer.textContent = message;
     document.body.append(alertContainer);
     setTimeout(() => {
@@ -44,7 +48,7 @@ const weight = document.querySelector("#weight");
 const genderMale = document.querySelector("#gender-male");
 const activityMinimal = document.querySelector("#activity-minimal");
 
-const clearField = () => {
+const clearParameters = () => {
     age.value = "";
     height.value = "";
     weight.value = "";
@@ -53,7 +57,7 @@ const clearField = () => {
 }
 
 const isValidateParameters = () => {
-    return age.value > ValidParameters.AGE_MIN && age.value < ValidParameters.AGE_MAX && height.value > ValidParameters.HEIGHT_MIN && height.value < ValidParameters.HEIGHT_MAX && weight.value > ValidParameter.WEIGHT_MIN && weight.value < ValidParameter.WEIGHT_MAX
+    return age.value > ValidParameter.AGE_MIN && age.value < ValidParameter.AGE_MAX && height.value > ValidParameter.HEIGHT_MIN && height.value < ValidParameter.HEIGHT_MAX && weight.value > ValidParameter.WEIGHT_MIN && weight.value < ValidParameter.WEIGHT_MAX
 }
 
 const isFilledParameters = () => {
@@ -64,5 +68,36 @@ const isExistParameter = () => {
     return age.value || height.value || weight.value;
 }
 
+const getNormMale = () => {
+    return (10 * parseInt(weight.value, 10) + (6.25 * parseInt(height.value, 10)) - (5 * parseInt(age.value, 10)) + 5);
+}
 
-export { isFilledParameters, isExistParameter, clearField, isValidateParameters, showError }
+const getNormFemale = () => {
+    return (10 * parseInt(weight.value, 10) + (6.25 * parseInt(height.value, 10)) - (5 * parseInt(age.value, 10)) - 161);
+}
+const getNorm = () => {
+    const genderMale = document.querySelector("#gender-male");
+    if (genderMale.checked) {
+        return getNormMale();
+    }
+    return getNormFemale();
+}
+
+const getActivityRate = () => {
+    const activityValue = document.querySelector('[name="activity"]:checked').value;
+    return ActivityRate[activityValue];
+}
+
+const getEnergyMaintainWeight = () => {
+    return Math.round(getActivityRate() * getNorm());
+}
+
+const getEnergyLossWeight = () => {
+    return Math.round(getEnergyMaintainWeight() - getEnergyMaintainWeight() * LOSS_WEIGHT_RATE);
+}
+
+const getEnergyGainWeight = () => {
+    return Math.round(getEnergyMaintainWeight() + getEnergyMaintainWeight() * GAIN_WEIGHT_RATE);
+}
+
+export {getEnergyMaintainWeight, getEnergyLossWeight, getEnergyGainWeight, isFilledParameters, isExistParameter, clearParameters, isValidateParameters, showError}
